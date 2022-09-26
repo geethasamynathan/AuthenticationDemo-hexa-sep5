57e423bbfc06499a0b44a603abd677d549135e89
geethasamynathan <geethasamynathan2011@gmail.com>
2022-09-26 12:56:16 +0530

    Add project files.

---
+++ AuthenticationDemo/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationDemo.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+}
